1a42f48568f44690ffeb5047bb88eea4cd9f6f12
Yash Sachan <122243243+YashSachan@users.noreply.github.com>
2023-06-12 14:29:34 -0400

    Add files via upload

---
+++ Menu/Menu/MenuView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    @State var menuItems:[MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
+    var body: some View {
+        
+        List(menuItems) { item in
+            
+            MenuListRow(item: item)
+            
+        }
+        .listStyle(.plain)
+        .onAppear {
+            // Call for the data
+            menuItems = dataService.getData()
+        }
+        
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
